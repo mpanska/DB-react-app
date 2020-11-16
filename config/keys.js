ea06d766022bbbdc78dee852f16b754b4700f4b2
Admin <47063770+mpanska@users.noreply.github.com>
2020-11-16 11:28:27 +0100

    init react app

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./production'); //if app is in production mode, we will export prod.js
-}else{
-    module.exports = require('./develop'); //else -> app is in developement mode
-}
